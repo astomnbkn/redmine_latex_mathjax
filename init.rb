65bbffd8774d293748387c68be59c2ef11657899
nobukuni <nobukuni@astom.co.jp>
2023-08-17 13:05:19 +0900

    Use cdn url, make xypics url configurable

--- init.rb
+++ init.rb
@@ -11,12 +11,14 @@ Redmine::Plugin.register :redmine_latex_mathjax do
   version '0.4.0'
 
   settings :default => {
-    'latex_mathjax_url' => '/plugin_assets/redmine_latex_mathjax/mathjax/es5/tex-chtml.js',
+    ##'latex_mathjax_url' => '/plugin_assets/redmine_latex_mathjax/mathjax/es5/tex-chtml.js',
+    'latex_mathjax_url' => 'https://cdn.jsdelivr.net/npm/mathjax@3.2.0/es5/tex-chtml.js',
     'latex_mathjax_inline_delimiter_start' => '$',
     'latex_mathjax_inline_delimiter_end' => '$',
     'latex_mathjax_block_delimiter_start' => '$$',
     'latex_mathjax_block_delimiter_end' => '$$',
-    'latex_mathjax_html_css_config' => "availableFonts: ['TeX']\n"
+    'latex_mathjax_html_css_config' => "availableFonts: ['TeX']\n",
+    'latex_mathjax_xyjax_url' => 'https://cdn.jsdelivr.net/gh/sonoisa/XyJax-v3@3.0.1/build/xypic.js'
   }, :partial => 'redmine_latex_mathjax/settings'
 
   Redmine::WikiFormatting::Macros.register do
@@ -75,5 +77,9 @@ class MathJaxEmbedMacro
 
   def self.URLToMathJax()
 	  return Setting.plugin_redmine_latex_mathjax['latex_mathjax_url'] || ""
+  end
+
+  def self.URLToXYJax()
+	  return Setting.plugin_redmine_latex_mathjax['latex_mathjax_xyjax_url'] || ""
   end
 end
